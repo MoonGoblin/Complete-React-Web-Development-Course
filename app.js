480ad9bd1c099b82b84e4eed14e52c823695a0b2
David Rytell <daverytell@gmail.com>
2018-05-19 20:08:19 -0700

    End of Video 12

--- app.js
+++ app.js
@@ -4,25 +4,35 @@ console.log("App.js is running!");
 // use title/subtitle in the template
 // render template
 
+// only render the subtitle (and p tag) if the subtitle exists - logical and operator
+// create a new property in app options: ["One", "Two"]
+// conditionally render a new p tag - set text on whether or not there are options - 
+// if options.length > 0 "Here are your options" else "No options" new paragraph tag below 
+// subtitle - ternary operator would be a great fit
+
 
 var app = {
     title: "Indecision App",
-    subTitle: "Put your life in your computer's hands!"
+    subTitle: "Put your life in your computer's hands!",
+    options: ["One", "Two"]
 }
 
 
 //JSX - JavaScript XML - a JavaScript syntax "extension" provided by REACT
 var template = (
 <div>
-    <h1>{app.title}</h1> 
-    <p>{app.subTitle}</p>
+    <h1>{app.title}</h1>
+    {app.subTitle && <p>{app.subTitle}</p>}
+    <p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>
+    <ol>
+    <li>Item one</li>
+    <li>Item two</li>
+    </ol>
 </div>
 );
 
-// <ol>
-// <li>Item one</li>
-// <li>Item two</li>
-// </ol>
+    //{(app.options && app.options.length > 0) && <p>Here are your options: {app.options}</p>}
+
 
 // Create a templateTwo var JSX expression
 // div 
@@ -61,4 +71,4 @@ var appRoot = document.getElementById("app");
 //It takes two arguments:
 //JSX you want to render
 //where you'd like to render it - the DOM element.
-ReactDOM.render(templateTwo, appRoot);
+ReactDOM.render(template, appRoot);
